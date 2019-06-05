92826854a746e19ea17a212cbe2923009ac01952
zhangcaochao <zhangcaochao30@gmail.com>
2019-06-05 14:51:08 +0800

    ISearch通用方法提交

--- src/main/java/com/znv/fssrqs/elasticsearch/service/ISearch.java
+++ src/main/java/com/znv/fssrqs/elasticsearch/service/ISearch.java
@@ -1,7 +1,34 @@
 package com.znv.fssrqs.elasticsearch.service;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+
 public interface ISearch {
 
+    /**
+     * 多条件and或者or关系，查找多个,最多查找max条
+     * @param map
+     * @param index
+     * @param type
+     * @param max(最多查找条数）
+     * @param and(and关系:true,or关系:false)
+     * @return
+     * @throws IOException
+     */
+    List<Map<String,Object>> searchList(Map<String,String> map,String index,String type,int max,Boolean and)throws IOException;
 
-
+    /**
+     * 多条件and或者or关系,分页查找
+     * @param map
+     * @param index
+     * @param type
+     * @param pageSize
+     * @param pageNum
+     * @param and(and关系:true,or关系:false)
+     * @return
+     * @throws IOException
+     */
+    Map<String,Object> searchListByPage(Map<String,String> map,String index,String type,int pageSize,int pageNum,Boolean and) throws IOException;
 }
---
+++ src/test/java/com/znv/fssrqs/elasticsearch/service/impl/ISearchImplTest.java
@@ -0,0 +1,45 @@
+package com.znv.fssrqs.elasticsearch.service.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ISearchImplTest {
+
+    @Autowired
+    private ISearchImpl iSearch;
+
+    @Test
+    public void searchList(){
+        Map<String,String> map = new HashMap<>();
+        map.put("person_id","0907103550091581");
+        map.put("country","美国");
+        try {
+            iSearch.searchList(map,"person_list_data_v1_2_copy","person_list",9,false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void searchListByPage(){
+        Map<String,String> map = new HashMap<>();
+        map.put("person_id","0907103550091581");
+        map.put("country","美国");
+        try {
+            iSearch.searchListByPage(map,"person_list_data_v1_2_copy","person_list",10,2,false);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
