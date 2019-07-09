0266cc43a7f297b2c8e23e94c4b3690882dc17b0
zhangcaochao <zhangcaochao30@gmail.com>
2019-07-09 17:11:24 +0800

    fix delete ids errors

--- src/main/java/com/znv/fssrqs/service/control/device/dto/CameraUnDeployDTO.java
+++ src/main/java/com/znv/fssrqs/service/control/device/dto/CameraUnDeployDTO.java
@@ -1,41 +1,20 @@
 package com.znv.fssrqs.service.control.device.dto;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE)
+@Data
 public class CameraUnDeployDTO {
 
-    private List<String> ids;
+    private List<String> Ids;
 
     private List<String> CameraIds;
     @NotNull(message = "库ID不能为空")
     private String LibId;
 
-    public List<String> getIds() {
-        return ids;
-    }
-
-    public void setIds(List<String> ids) {
-        this.ids = ids;
-    }
-
-    public List<String> getCameraIds() {
-        return CameraIds;
-    }
-
-    public void setCameraIds(List<String> cameraIds) {
-        CameraIds = cameraIds;
-    }
-
-    public String getLibId() {
-        return LibId;
-    }
-
-    public void setLibId(String libId) {
-        LibId = libId;
-    }
 }
