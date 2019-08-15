5e18459270316509e0367459af124b6cf356f11e
0049003174@znv.com <0049003174@znv.com>
2019-08-15 15:18:05 +0800

    增加生成机器码接口

---
+++ src/main/java/com/znv/fssrqs/controller/MachineCodeController.java
@@ -0,0 +1,32 @@
+package com.znv.fssrqs.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.znv.fssrqs.util.FastJsonUtils;
+import com.znv.fssrqs.util.HardWare;
+import com.znv.fssrqs.util.MD5Util;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by dongzelong on  2019/8/15 15:13.
+ *
+ * @author dongzelong
+ * @version 1.0
+ * @Description TODO
+ */
+@RestController
+public class MachineCodeController {
+    @GetMapping("/generate/machine/code")
+    public JSONObject code() {
+        String str = "";
+        try {
+            String bs = HardWare.getHardInfo().getBaseBorderSerinumber();
+            String cpu = HardWare.getHardInfo().getCpuSerinumber();
+            String mac = HardWare.getHardInfo().getNetSerinumber();
+            str = String.format("%s%s%s", bs, cpu, mac);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return FastJsonUtils.JsonBuilder.ok().property("code", MD5Util.encode(str)).json();
+    }
+}
