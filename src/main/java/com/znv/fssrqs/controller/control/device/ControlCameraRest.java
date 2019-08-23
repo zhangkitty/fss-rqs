6969842be45beef7c63a7135fc1e1d2f52855124
0049003174@znv.com <0049003174@znv.com>
2019-08-23 17:00:37 +0800

    修改bug

--- src/main/java/com/znv/fssrqs/controller/control/device/ControlCameraRest.java
+++ src/main/java/com/znv/fssrqs/controller/control/device/ControlCameraRest.java
@@ -13,7 +13,7 @@ import javax.validation.Valid;
 
 
 @RestController
-@RequestMapping(produces = { "application/json;charset=UTF-8" })
+@RequestMapping(produces = {"application/json;charset=UTF-8"})
 public class ControlCameraRest {
 
     @Autowired
@@ -21,37 +21,40 @@ public class ControlCameraRest {
 
     /**
      * 新增布控
+     *
      * @param cameraControlDto
      * @return
      */
     @PostMapping("/CameraControl")
-    public ResponseVo addControl(@Valid @RequestBody CameraControlDTO cameraControlDto){
-        return ResponseVo.success("布控成功",controlCameraService.deployControl(cameraControlDto));
+    public ResponseVo addControl(@Valid @RequestBody CameraControlDTO cameraControlDto) {
+        return ResponseVo.success("布控成功", controlCameraService.deployControl(cameraControlDto));
     }
 
     /**
      * 修改布控
+     *
      * @param cameraControlDto
      * @return
      */
     @PutMapping("/CameraControl")
-    public ResponseVo editControl(@Valid @RequestBody CameraControlDTO cameraControlDto){
-        if(cameraControlDto.getId()==null || cameraControlDto.getId().trim()==""){
+    public ResponseVo editControl(@Valid @RequestBody CameraControlDTO cameraControlDto) {
+        if (cameraControlDto.getId() == null || cameraControlDto.getId().trim().equals("")) {
             throw new BusinessException(ErrorCodeEnum.PARAM_ILLEGAL);
         }
-        if(cameraControlDto.getCameraId()==null||cameraControlDto.getCameraId()==""){
+        if (cameraControlDto.getCameraId() == null || cameraControlDto.getCameraId().equals("")) {
             throw new BusinessException(ErrorCodeEnum.PARAM_ILLEGAL);
         }
-        return ResponseVo.success("更新成功",controlCameraService.editDeployControl(cameraControlDto));
+        return ResponseVo.success("更新成功", controlCameraService.editDeployControl(cameraControlDto));
     }
 
     /**
      * 删除布控
+     *
      * @param cameraUnDeployDTO
      * @return
      */
     @DeleteMapping("/CameraControl")
-    public ResponseVo deleteControl(@RequestBody CameraUnDeployDTO cameraUnDeployDTO){
+    public ResponseVo deleteControl(@RequestBody CameraUnDeployDTO cameraUnDeployDTO) {
         controlCameraService.unDeployControl(cameraUnDeployDTO);
         return ResponseVo.success(null);
     }
