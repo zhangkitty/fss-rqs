070f7834d430b7fab3d9111501defbf0e6cfb226
0049003174@znv.com <0049003174@znv.com>
2019-09-03 16:38:29 +0800

    增加设备抓拍统计接口

--- src/main/java/com/znv/fssrqs/controller/HomeController.java
+++ src/main/java/com/znv/fssrqs/controller/HomeController.java
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Sets;
 import com.znv.fssrqs.dao.mysql.LibDao;
 import com.znv.fssrqs.elasticsearch.homepage.AlarmTopLibCountService;
+import com.znv.fssrqs.elasticsearch.homepage.DeviceCaptureService;
 import com.znv.fssrqs.elasticsearch.homepage.HistoryAlarmDataService;
 import com.znv.fssrqs.util.FastJsonUtils;
 import lombok.extern.slf4j.Slf4j;
@@ -31,6 +32,8 @@ public class HomeController {
     private AlarmTopLibCountService alarmTopLibCountService;
     @Autowired
     private HistoryAlarmDataService historyAlarmDataService;
+    @Autowired
+    private DeviceCaptureService deviceCaptureService;
     @Resource
     private LibDao libDao;
 
@@ -90,103 +93,14 @@ public class HomeController {
         String remoteIp = host.split(":")[0];
         final JSONObject historyAlarmList = historyAlarmDataService.getHistoryAlarmList(params, remoteIp);
         return historyAlarmList;
-//        //查询案件名称
-//        Map<String, String> map = getFaceEventList();
-//        Map<String, ProtocolNode> libMap = getLibMap();
+    }
 
-//        JSONArray hits = esResult.getJSONArray("hits");
-//        int length = hits.size();
-//        List<JSONObject> list = new ArrayList<>();
-//        for (int i = 0; i < length; i++) {
-//            JSONObject data = (JSONObject) hits.get(i);
-//            //人员ID
-//            String personId = data.getString(CommonConsts.FinalKeyCode.PERSON_ID);
-//            //人员名称
-//            String personName = data.getString(CommonConsts.FinalKeyCode.PERSON_NAME);
-//            //事件ID
-//            String eventId = data.getString(CommonConsts.FinalKeyCode.CONTROL_EVENT_ID);
-//            //事件名称
-//            String eventName = "";
-//            if (map.containsKey(eventId)) {
-//                //事件名称
-//                eventName = map.get(eventId);
-//            }
-//
-//            //相似度
-//            String similarity = data.getString(CommonConsts.FinalKeyCode.SIMILARITY);
-//            //人员库ID
-//            int libId = data.getInteger("lib_id");
-//            String libName = "";
-//            //名单库告警类型
-//            String pLibAlarmLevel = "";
-//            if (libMap.containsKey(String.valueOf(libId))) {
-//                ProtocolNode protocolNode = libMap.get(String.valueOf(libId));
-//                libName = protocolNode.getAttribute("lib_name");
-//                pLibAlarmLevel = protocolNode.getAttribute("plib_alarm_level");
-//            }
-//
-//            String controlCommunityId = data.getString(CommonConsts.FinalKeyCode.CONTROL_COMMUNITY_ID);
-//            String officeId = data.getString("office_id");
-//            String officeName = data.getString("office_name");
-//            //证件号ID
-//            String certificateId = data.getString("card_id");
-//            String cameraId = data.getString("camera_id");
-//            String imgWidth = data.getString("img_width");
-//            String imgHeight = data.getString("img_height");
-//            String cameraName = data.getString("camera_name");
-//            String enterTime = data.getString("enter_time");
-
-//
-//            //告警小图片
-//            String smallUuid = data.getString("img_url");
-//            String imgUrl = "";
-//            if (!("null".equals(smallUuid) || StringUtils.isEmpty(smallUuid))) {
-//                imgUrl = ImageUtils.getImgUrl(remoteIp, "GetSmallPic", smallUuid);
-//            } else {
-//                imgUrl = "";
-//            }
-//
-//            JSONObject jsonObject = new JSONObject();
-//            jsonObject.put("personId", personId);
-//            jsonObject.put("personName", personName);
-//            jsonObject.put("eventId", eventId);
-//            jsonObject.put("eventName", eventName);
-//            jsonObject.put("similarity", similarity);
-//            jsonObject.put("personImg", personImg);
-//            jsonObject.put("certificateId", certificateId);
-//            jsonObject.put("alarmType", pLibAlarmLevel);
-//            jsonObject.put("libId", libId);
-//            jsonObject.put("libName", libName);
-//            jsonObject.put("controlCommunityId", controlCommunityId);
-//            jsonObject.put("officeId", officeId);
-//            jsonObject.put("officeName", officeName);
-//            jsonObject.put("cameraId", cameraId);
-//            jsonObject.put("cameraName", cameraName);
-//            jsonObject.put("imgWidth", imgWidth);
-//            jsonObject.put("imgHeight", imgHeight);
-//            jsonObject.put("imgUrl", imgUrl);
-//            jsonObject.put("enterTime", enterTime);
-//            if (data.containsKey("sex")) {
-//                jsonObject.put("sex", data.getString("sex"));
-//            }
-//
-//            // 马拉松看板告警页面临时加字段
-//            jsonObject.put(CommonConsts.FinalKeyCode.EYEBROW_STYLE, data.getString(CommonConsts.FinalKeyCode.EYEBROW_STYLE));
-//            jsonObject.put(CommonConsts.FinalKeyCode.NOSE_STYLE, data.getString(CommonConsts.FinalKeyCode.NOSE_STYLE));
-//            jsonObject.put(CommonConsts.FinalKeyCode.MUSTACHE_STYLE, data.getString(CommonConsts.FinalKeyCode.MUSTACHE_STYLE));
-//            jsonObject.put(CommonConsts.FinalKeyCode.LIP_STYLE, data.getString(CommonConsts.FinalKeyCode.LIP_STYLE));
-//            jsonObject.put(CommonConsts.FinalKeyCode.WRINKLE_POUCH, data.getString(CommonConsts.FinalKeyCode.WRINKLE_POUCH));
-//            jsonObject.put(CommonConsts.FinalKeyCode.ACNE_STAIN, data.getString(CommonConsts.FinalKeyCode.ACNE_STAIN));
-//            jsonObject.put(CommonConsts.FinalKeyCode.FRECKLE_BIRTHMARK, data.getString(CommonConsts.FinalKeyCode.FRECKLE_BIRTHMARK));
-//            jsonObject.put(CommonConsts.FinalKeyCode.SCAR_DIMPLE, data.getString(CommonConsts.FinalKeyCode.SCAR_DIMPLE));
-//            String bigPictureUuid = data.getString("big_picture_uuid");
-//            if (!("null".equals(bigPictureUuid) || StringUtils.isEmpty(bigPictureUuid))) {
-//                jsonObject.put("BigPictureUrl", ImageUtils.getImgUrl(remoteIp, "GetBigBgPic", bigPictureUuid));
-//            } else {
-//                jsonObject.put("BigPictureUrl", "");
-//            }
-//            list.add(jsonObject);
-//        }
-//        return FastJsonUtils.JsonBuilder.ok().list(list).property("count", esResult.get("total")).json();
+    /**
+     * 设备抓拍统计
+     * 0-日,1-自然周,2-自然月
+     */
+    @PostMapping("/device/capture/statistics")
+    public JSONObject getHistoryDeviceCapture(@RequestBody JSONObject params) {
+        return deviceCaptureService.getDeviceCaptureList(params);
     }
 }
---
+++ src/main/java/com/znv/fssrqs/elasticsearch/homepage/DeviceCaptureService.java
@@ -0,0 +1,107 @@
+package com.znv.fssrqs.elasticsearch.homepage;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.znv.fssrqs.config.HdfsConfigManager;
+import com.znv.fssrqs.constant.CommonConstant;
+import com.znv.fssrqs.elasticsearch.ElasticSearchClient;
+import com.znv.fssrqs.exception.ZnvException;
+import com.znv.fssrqs.util.DataConvertUtils;
+import com.znv.fssrqs.util.FastJsonUtils;
+import com.znv.fssrqs.util.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Objects;
+
+/**
+ * Created by dongzelong on  2019/9/3 10:07.
+ *
+ * @author dongzelong
+ * @version 1.0
+ * @Description TODO
+ */
+@Service
+@Slf4j
+public class DeviceCaptureService {
+    @Resource
+    private ElasticSearchClient elasticSearchClient;
+    private final int DAY = 0; //天
+    private final int WEEK = 1; //周
+    private final int MONTH = 2; //月
+
+    /**
+     * @param params
+     * @return
+     */
+    public JSONObject getDeviceCaptureList(JSONObject params) {
+        int timeType = params.getIntValue("TimeType");
+        String startTime;
+        String endTime;
+        switch (timeType) {
+            case DAY:
+                startTime = DataConvertUtils.dateToStr(DataConvertUtils.getTimesmorning(), DataConvertUtils.DEFAULT_DATE_TIME_FORMAT);
+                endTime = DataConvertUtils.dateToStr(DataConvertUtils.getTimesnight(), DataConvertUtils.DEFAULT_DATE_TIME_FORMAT);
+                break;
+            case WEEK:
+                startTime = DataConvertUtils.dateToStr(DataConvertUtils.getTimesWeekmorning(), DataConvertUtils.DEFAULT_DATE_TIME_FORMAT);
+                endTime = DataConvertUtils.dateToStr(DataConvertUtils.getTimesWeeknight(), DataConvertUtils.DEFAULT_DATE_TIME_FORMAT);
+                break;
+            case MONTH:
+                startTime = DataConvertUtils.dateToStr(DataConvertUtils.getTimesMonthmorning(), DataConvertUtils.DEFAULT_DATE_TIME_FORMAT);
+                endTime = DataConvertUtils.dateToStr(DataConvertUtils.getTimesMonthnight(), DataConvertUtils.DEFAULT_DATE_TIME_FORMAT);
+                break;
+            default:
+                startTime = DataConvertUtils.dateToStr(DataConvertUtils.getTimesmorning(), DataConvertUtils.DEFAULT_DATE_TIME_FORMAT);
+                endTime = DataConvertUtils.dateToStr(DataConvertUtils.getTimesnight(), DataConvertUtils.DEFAULT_DATE_TIME_FORMAT);
+                break;
+        }
+
+        JSONObject inputParams = new JSONObject();
+        inputParams.put("camera_id", params.getJSONArray("CameraIDs"));
+        inputParams.put("enter_time_start", startTime);
+        inputParams.put("enter_time_end", endTime);
+        inputParams.put("camera_top_num", params.getIntValue("Top"));
+        final Result<JSONObject, String> result = sendRequest(inputParams);
+        if (result.isErr()) {
+            throw ZnvException.error(CommonConstant.StatusCode.INTERNAL_ERROR, result.error());
+        }
+
+        JSONObject jsonObject = result.value();
+        return getSearchResult(jsonObject);
+    }
+
+    public Result<JSONObject, String> sendRequest(JSONObject inputParams) {
+        String url = HdfsConfigManager.getString(CommonConstant.ElasticSearch.ES_INDEX_HISTORY_NAME) + "/" + HdfsConfigManager.getString(CommonConstant.ElasticSearch.ES_INDEX_HISTORY_TYPE);
+        if (Objects.nonNull(inputParams.getJSONArray("camera_id")) && !inputParams.getJSONArray("camera_id").isEmpty()) {
+            inputParams.put("is_camera", true);
+        }
+
+        inputParams.put("from", 0);
+        inputParams.put("size", 0);
+        inputParams.put("camera_aggregation", true);
+        JSONObject templateParams = new JSONObject();
+        String templateName = HdfsConfigManager.getString(CommonConstant.ElasticSearch.ES_SEARCH_TEMPLATE_HISTORY_CAMEMA_TAKE_COUNT);
+        templateParams.put("id", templateName);
+        templateParams.put("params", inputParams);
+        return elasticSearchClient.postRequest(url, templateParams);
+    }
+
+    protected JSONObject getSearchResult(JSONObject jsonObject) {
+        int took = jsonObject.getIntValue("took");
+        String total = jsonObject.getJSONObject("hits").getString("total");
+        JSONArray agg = jsonObject.getJSONObject("aggregations").getJSONObject("group_by_camera").getJSONArray("buckets");
+        JSONArray hits = new JSONArray();
+        if (null != agg && agg.size() != 0) {
+            for (int i = 0; i < agg.size(); i++) {
+                JSONObject topHit = (JSONObject) agg.getJSONObject(i).getJSONObject("camera_hits").getJSONObject("hits").getJSONArray("hits").get(0);
+                JSONObject source = topHit.getJSONObject("_source");
+                source.put("CameraID", agg.getJSONObject(i).getString("key"));
+                source.put("DocCount", agg.getJSONObject(i).getString("doc_count"));
+                hits.add(i, source);
+            }
+        }
+        return FastJsonUtils.JsonBuilder.ok().list(hits).property("Total", total).property("Took", took).json();
+    }
+}
