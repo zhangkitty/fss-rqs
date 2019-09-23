f7b9ba47de85c9e795d38da6492f770ff968b813
0049003174@znv.com <0049003174@znv.com>
2019-09-23 11:26:31 +0800

    修改战果导出Size判断

--- src/main/java/com/znv/fssrqs/controller/FileController.java
+++ src/main/java/com/znv/fssrqs/controller/FileController.java
@@ -1,5 +1,7 @@
 package com.znv.fssrqs.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.znv.fssrqs.constant.CommonConstant;
 import com.znv.fssrqs.constant.ExcelResource;
 import com.znv.fssrqs.exception.ZnvException;
@@ -7,7 +9,6 @@ import com.znv.fssrqs.util.DataConvertUtils;
 import com.znv.fssrqs.util.SpringContextUtil;
 import com.znv.fssrqs.util.file.AlarmXlsOutput;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
@@ -52,11 +53,8 @@ public class FileController {
         AlarmXlsOutput output = null;
         switch (resType) {
             case 1:
-                final String size = request.getParameter("Size");
-                if (StringUtils.isEmpty(size) || size.length() != 1 || (size.compareTo("0") < 0 || size.compareTo("9") > 0)) {
-                    throw ZnvException.badRequest("导出条数为非法参数");
-                }
-                final int exportSize = Integer.parseInt(size);
+                final JSONObject jsonObject = JSON.parseObject(body);
+                final int exportSize = jsonObject.getIntValue("Size");
                 HistoryAlarmController historyAlarmController = SpringContextUtil.getCtx().getBean(HistoryAlarmController.class);
                 String result = historyAlarmController.getHistoryAlarm(host, body);
                 output = new AlarmXlsOutput(response, locale, result, body, exportSize);
