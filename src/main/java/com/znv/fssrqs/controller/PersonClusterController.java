11fb289bf3e8e8c90e5dcdbcfe955843750cecd2
0049003174@znv.com <0049003174@znv.com>
2019-09-24 13:57:59 +0800

    增加归档详情接口

--- src/main/java/com/znv/fssrqs/controller/PersonClusterController.java
+++ src/main/java/com/znv/fssrqs/controller/PersonClusterController.java
@@ -3,11 +3,11 @@ package com.znv.fssrqs.controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.znv.fssrqs.elasticsearch.person.cluster.PersonClusterService;
+import com.znv.fssrqs.elasticsearch.person.cluster.PersonDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * Created by dongzelong on  2019/9/6 12:45.
@@ -18,9 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class PersonClusterController {
-
     @Autowired
     private PersonClusterService personClusterService;
+    @Autowired
+    private PersonDetailService personDetailService;
 
     /**
      * 人员聚类融合统计查询
@@ -34,5 +35,11 @@ public class PersonClusterController {
     @PostMapping("/ReID/cluster/track/search")
     public JSONObject getPersonFusedDetail(@RequestBody String body) {
         return null;
+    }
+
+    @GetMapping("/ReID/cluster/fused/{fusedId}/detail")
+    public JSONObject getReidFusedDetail(@PathVariable(value = "fusedId",required = true) String fusedId, @RequestParam Map<String, Object> params) {
+        params.put("FusedID", fusedId);
+        return personDetailService.getPersonDetail(params);
     }
 }
